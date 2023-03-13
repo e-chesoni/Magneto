704e63c60b8b0fe17623ad6d86da2db311772971
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-03-13 14:26:17 -0400

    organizing files

---
+++ source/MagnetoLibrary/IClientApplicationSetup.cs
@@ -0,0 +1,17 @@
+using System;
+
+/// <summary>
+/// Core functionality shared across application
+/// </summary>
+namespace MagnetoLibrary
+{
+    /// <summary>
+    /// Entry point into Magneto application
+    /// Used to setup environment
+    /// Top-Level application creates an instance of this
+    /// </summary>
+    public interface IClientApplicationSetup
+    {
+
+    }
+}
