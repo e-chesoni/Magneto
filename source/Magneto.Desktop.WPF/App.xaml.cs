2c8364d5a20faeeb221eca524999ee5c518a165e
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-04-12 19:52:07 -0400

    Added WFP App
    Copied serial console method calls into WPF app. Tested and working. Also launches console app when WPF app launches.

---
+++ source/Magneto.Desktop.WPF/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MagnetoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+// Magneto Library
+
+namespace Magneto.Desktop.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            // Your code here
+            // This will be executed during the app's teardown
+            Console.WriteLine("Doing teardown stuff...");
+            SerialConsole.CloseSerialPort();
+        }
+    }
+}
---
+++ source/Magneto.Desktop.WPF/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// Import Magneto Lib Classes
+using MagnetoLibrary;
+using MagnetoLibrary.Interfaces;
+using MagnetoLibrary.Motor;
+
+namespace Magneto.Desktop.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeMagneto();
+        }
+
+        private void InitializeMagneto()
+        {
+            SerialConsole.SetDefaultSerialPort();
+        }
+
+        private void MoveMotorButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Moving Motor!");
+
+            if (SerialConsole.OpenSerialPort())
+            {
+                // Write hard-coded move command
+                SerialConsole.SerialWrite("1MVA20"); // success!
+            }
+            else
+            {
+                MessageBox.Show("Cannot complete the mission. Try again later.");
+            }
+        }
+
+        private void HomeMotorButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Homing Motor.");
+
+            if (SerialConsole.OpenSerialPort())
+            {
+                // Write hard-coded move command
+                SerialConsole.SerialWrite("1MVA1"); // success!
+            }
+            else
+            {
+                MessageBox.Show("Cannot complete the mission. Try again later.");
+            }
+        }
+    }
+}
