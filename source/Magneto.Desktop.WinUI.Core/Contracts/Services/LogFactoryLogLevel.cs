2228640df4b69a0cbb1c488e4953c26be635687f
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-05-14 20:46:47 -0400

    WIP Adding Interfaces for Logger

---
+++ source/Magneto.Desktop.WinUI.Core/Contracts/Services/LogFactoryLogLevel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magneto.Desktop.WinUI.Core.Contracts.Services;
+public class LogFactoryLogLevel
+{
+    /// <summary>
+    /// The severity of the log message
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Developer-specific information
+        /// </summary>
+        DEBUG = 1,
+
+        /// <summary>
+        /// Verbose information
+        /// </summary>
+        VERBOSE = 2,
+
+        /// <summary>
+        /// General information
+        /// </summary>
+        WARN = 3,
+
+        /// <summary>
+        /// An error
+        /// </summary>
+        ERROR = 4,
+
+        /// <summary>
+        /// A success
+        /// </summary>
+        SUCCES = 5,
+    }
+}
---
+++ source/Magneto.Desktop.WinUI.Core/Contracts/Services/LogFactoryOutputLevel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magneto.Desktop.WinUI.Core.Contracts.Services;
+public class LogFactoryOutputLevel
+{
+    /// <summary>
+    /// The level of details to output for a logger
+    /// </summary>
+    public enum LogOutputLevel
+    {
+        /// <summary>
+        /// Logs everything
+        /// </summary>
+        Debug = 1,
+
+        /// <summary>
+        /// Lot all information except debug information
+        /// </summary>
+        Verbose = 2,
+
+        /// <summary>
+        /// Logs all informative message, ignoring any debug and verbose messages
+        /// </summary>
+        Informative = 3,
+
+        /// <summary>
+        /// Log only critical errors and warnings and success, but no general information
+        /// </summary>
+        Critical = 4,
+
+        /// <summary>
+        /// The logger will never output anything
+        /// </summary>
+        Nothing = 7,
+    }
+}
