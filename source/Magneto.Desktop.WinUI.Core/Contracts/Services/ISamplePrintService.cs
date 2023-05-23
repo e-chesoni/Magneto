381ec464d5d74cdfaf3c40a12fe9040d14e465c9
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-05-23 12:05:29 -0400

    Implementing Page Design
    Fixed test print page
    Implemented core design for print pages
    Next steps: Bind data to print status (settings) page
    This will require implementing monitor data models and/or dummy data

---
+++ source/Magneto.Desktop.WinUI.Core/Contracts/Services/ISamplePrintService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Magneto.Desktop.WinUI.Core.Models;
+
+namespace Magneto.Desktop.WinUI.Core.Contracts.Services;
+
+// Remove this class once your pages/features are using your data.
+public interface ISamplePrintService
+{
+    Task<IEnumerable<SamplePrint>> GetGridDataAsync();
+
+    Task<IEnumerable<SamplePrint>> GetContentGridDataAsync();
+
+    Task<IEnumerable<SamplePrint>> GetListDetailsDataAsync();
+}
---
+++ source/Magneto.Desktop.WinUI.Core/Models/SamplePrint.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magneto.Desktop.WinUI.Core.Models;
+public class SamplePrint
+{
+    public enum PrintStatus : ushort
+    {
+        NotStarted,
+        InProgress,
+        Paused,
+        Canceled,
+        Complete
+    }
+
+    public string FileName
+    {
+        get; set;
+    }
+
+    public string FileLocation
+    {
+        get; set;
+    }
+
+    public int FileSize
+    {
+        get; set;
+    }
+
+    public DateTime CreatedAt
+    {
+        get; set;
+    }
+
+    #region Symbols
+
+    public PrintStatus Status
+    {
+        get; set;
+    }
+
+    public int SymbolCode
+    {
+        get; set;
+    }
+
+    public string SymbolName
+    {
+        get; set;
+    }
+
+    public char Symbol => (char)SymbolCode;
+
+    #endregion
+}
