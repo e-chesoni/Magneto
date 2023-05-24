6572d479cd4724857a8dc3eae5dde7e4ad27e60d
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-05-24 15:18:20 -0400

    Made ImageHandler Static
    Just handles slicing image; do not need to instantiate a class for this (can just pass the image and store slices in the the image class -- which is a wrapper)

--- source/Magneto.Desktop.WinUI.Core/Models/Image/ImageHandler.cs
+++ source/Magneto.Desktop.WinUI.Core/Models/Image/ImageHandler.cs
@@ -5,6 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Magneto.Desktop.WinUI.Core.Models.Image;
-public class ImageHandler
+
+/// <summary>
+/// Class to handle image processing (i.e. slicing an image)
+/// </summary>
+public static class ImageHandler
 {
+    /// <summary>
+    /// Slices image
+    /// </summary>
+    /// <returns></returns> Returns a list of sliced images
+    public static List<Image> SliceImage(Image img)
+    {
+        List<Image> image_slices = new List<Image>();
+
+        // TODO: Slice image and add slices to image_slices
+
+        return image_slices;
+    }
 }
