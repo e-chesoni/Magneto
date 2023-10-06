798ac169c6d00b1644d872b8b5257a05ae0ecaec
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-10-06 15:31:57 -0400

    Tested Reading Serial Ports
    Not getting any data. Need to test in lab.

--- source/SerialCommunicationPOC/Program.cs
+++ source/SerialCommunicationPOC/Program.cs
@@ -11,6 +11,7 @@ internal class Program
 {
     static SerialPort _serialPort;
     static bool _success;
+    static bool _continue;
 
     private static void ListKnownPorts()
     {
@@ -21,9 +22,145 @@ internal class Program
         }
     }
 
+    public static void ReadPort()
+    {
+        while (_continue)
+        {
+            try
+            {
+                var message = _serialPort.ReadLine();
+                Console.WriteLine(message);
+            }
+            catch (TimeoutException) 
+            {
+                Console.WriteLine("Timeout occurred before reading port.");
+            }
+        }
+    }
+
+    // Display Port values and prompt user to enter a port.
+    public static string SetPortName(string defaultPortName)
+    {
+        string portName;
+
+        Console.WriteLine("Available Ports:");
+        foreach (var s in SerialPort.GetPortNames())
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter COM port value (Default: {0}): ", defaultPortName);
+        portName = Console.ReadLine();
+
+        if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+        {
+            portName = defaultPortName;
+        }
+        return portName;
+    }
+
+    // Display BaudRate values and prompt user to enter a value.
+    public static int SetPortBaudRate(int defaultPortBaudRate)
+    {
+        string baudRate;
+
+        Console.Write("Baud Rate(default:{0}): ", defaultPortBaudRate);
+        baudRate = Console.ReadLine();
+
+        if (baudRate == "")
+        {
+            baudRate = defaultPortBaudRate.ToString();
+        }
+
+        return int.Parse(baudRate);
+    }
+
+    // Display PortParity values and prompt user to enter a value.
+    public static Parity SetPortParity(Parity defaultPortParity)
+    {
+        string parity;
+
+        Console.WriteLine("Available Parity options:");
+        foreach (var s in Enum.GetNames(typeof(Parity)))
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter Parity value (Default: {0}):", defaultPortParity.ToString(), true);
+        parity = Console.ReadLine();
+
+        if (parity == "")
+        {
+            parity = defaultPortParity.ToString();
+        }
+
+        return (Parity)Enum.Parse(typeof(Parity), parity, true);
+    }
+
+    // Display DataBits values and prompt user to enter a value.
+    public static int SetPortDataBits(int defaultPortDataBits)
+    {
+        string dataBits;
+
+        Console.Write("Enter DataBits value (Default: {0}): ", defaultPortDataBits);
+        dataBits = Console.ReadLine();
+
+        if (dataBits == "")
+        {
+            dataBits = defaultPortDataBits.ToString();
+        }
+
+        return int.Parse(dataBits.ToUpperInvariant());
+    }
+
+    // Display StopBits values and prompt user to enter a value.
+    public static StopBits SetPortStopBits(StopBits defaultPortStopBits)
+    {
+        string stopBits;
+
+        Console.WriteLine("Available StopBits options:");
+        foreach (var s in Enum.GetNames(typeof(StopBits)))
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter StopBits value (None is not supported and \n" +
+         "raises an ArgumentOutOfRangeException. \n (Default: {0}):", defaultPortStopBits.ToString());
+        stopBits = Console.ReadLine();
+
+        if (stopBits == "")
+        {
+            stopBits = defaultPortStopBits.ToString();
+        }
+
+        return (StopBits)Enum.Parse(typeof(StopBits), stopBits, true);
+    }
+
+    // Display Handshake value and prompt user to ender a value
+    public static Handshake SetPortHandshake(Handshake defaultPortHandshake)
+    {
+        string handshake;
+
+        Console.WriteLine("Available Handshake options:");
+        foreach (string s in Enum.GetNames(typeof(Handshake)))
+        {
+            Console.WriteLine("   {0}", s);
+        }
+
+        Console.Write("Enter Handshake value (Default: {0}):", defaultPortHandshake.ToString());
+        handshake = Console.ReadLine();
+
+        if (handshake == "")
+        {
+            handshake = defaultPortHandshake.ToString();
+        }
+
+        return (Handshake)Enum.Parse(typeof(Handshake), handshake, true);
+    }
+
     private static void SetupSerialPort()
     {
-    
+        
     }
 
     static void Main(string[] args)
@@ -33,6 +170,52 @@ internal class Program
         ListKnownPorts();
 
         //TODO: Do other stuffs
+        string name;
+        string message;
+        StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        Thread readThread = new Thread(ReadPort);
+
+        // Create a new SerialPort object with default settings.
+        _serialPort = new SerialPort();
+
+        // Allow the user to set the appropriate properties.
+        _serialPort.PortName = SetPortName(_serialPort.PortName);
+        _serialPort.BaudRate = SetPortBaudRate(_serialPort.BaudRate);
+        _serialPort.Parity = SetPortParity(_serialPort.Parity);
+        _serialPort.DataBits = SetPortDataBits(_serialPort.DataBits);
+        _serialPort.StopBits = SetPortStopBits(_serialPort.StopBits);
+        _serialPort.Handshake = SetPortHandshake(_serialPort.Handshake);
+
+        // Set the read/write timeouts
+        _serialPort.ReadTimeout = 1000;
+        _serialPort.WriteTimeout = 1000;
+
+        _serialPort.Open();
+        _continue = true;
+        readThread.Start();
+
+        Console.Write("Name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("PRESS CMD + C or TYPE QUIT to exit");
+
+        while (_continue)
+        {
+            message = Console.ReadLine();
+
+            if (stringComparer.Equals("quit", message))
+            {
+                _continue = false;
+            }
+            else
+            {
+                _serialPort.WriteLine(
+                    $"<{name}>: {message}");
+            }
+        }
+
+        readThread.Join();
+        _serialPort.Close();
 
         Console.WriteLine("Closing Serial Communication POC.");
     }
