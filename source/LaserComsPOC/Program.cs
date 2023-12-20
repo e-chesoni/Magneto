9949ff1adc2da68127d6012d9b9af80fc808d8a0
Elaine Chesoni <elaine.chesoni@gmail.com>
2023-12-19 19:59:06 -0500

    Added POC for SamLight/Laser Communication
    "Hello SamLight" test command successfully sends when WaveRunner is open

---
+++ source/LaserComsPOC/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using SAMLIGHT_CLIENT_CTRL_EXLib;
+
+namespace LaserComsPOC;
+
+internal class Program
+{
+    static ScSamlightClientCtrlEx ctrlNew = new ScSamlightClientCtrlEx();
+
+    static void cci_hello_world()
+    {
+        try
+        {
+            // show hello world message box in samlight
+            ctrlNew.ScExecCommand((int)ScComSAMLightClientCtrlExecCommandConstants.scComSAMLightClientCtrlExecCommandTest);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("CCI Error! \n" + Convert.ToString(exception));
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, SamLight Console App!");
+        cci_hello_world();
+
+    }
+}
