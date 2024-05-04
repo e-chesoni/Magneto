f0265fc6885aafee031ddd934bedca4a1931f563
Elaine Chesoni <elaine.chesoni@gmail.com>
2024-05-03 20:17:30 -0400

    MotorQueuePOC Splitting Code into Different FIles

---
+++ source/MotorQueuePOC/Motor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Motor
+{
+    public int MotorId
+    {
+        get; private set;
+    }
+    public int Position
+    {
+        get; private set;
+    }
+
+    public Motor(int motorId)
+    {
+        MotorId = motorId;
+        Position = 0; // Initial position
+    }
+
+    public async Task ExecuteCommand(string command, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Motor {MotorId}: Executing command {command}");
+        int desiredPosition = int.Parse(command.Replace("Move to position ", ""));
+        while (Position != desiredPosition && !cancellationToken.IsCancellationRequested)
+        {
+            Position += (Position < desiredPosition) ? 10 : -10;
+            Console.WriteLine($"Motor {MotorId}: Moving to {Position}");
+            await Task.Delay(100, cancellationToken);
+        }
+        Console.WriteLine($"Motor {MotorId}: Command {command} completed at position {Position}");
+    }
+}
---
+++ source/MotorQueuePOC/MotorController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class MotorCommand
+{
+    public int MotorId
+    {
+        get; set;
+    }
+    public string Command
+    {
+        get; set;
+    }
+}
+
+public class MotorController
+{
+    private Dictionary<int, Motor> motors = new Dictionary<int, Motor>();
+    private Queue<MotorCommand> commandQueue = new Queue<MotorCommand>();
+    private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+    private bool isCommandProcessing = false;
+
+    public MotorController()
+    {
+        motors.Add(1, new Motor(1));
+        motors.Add(2, new Motor(2));
+    }
+
+    public void AddCommand(int motorId, string command)
+    {
+        lock (commandQueue)
+        {
+            commandQueue.Enqueue(new MotorCommand { MotorId = motorId, Command = command });
+            if (!isCommandProcessing)
+            {
+                isCommandProcessing = true;
+                Task.Run(() => ProcessCommands());
+            }
+        }
+    }
+
+    private async Task ProcessCommands()
+    {
+        while (commandQueue.Count > 0 && !cancellationTokenSource.IsCancellationRequested)
+        {
+            MotorCommand command;
+            lock (commandQueue)
+            {
+                command = commandQueue.Dequeue();
+            }
+            if (motors.TryGetValue(command.MotorId, out Motor motor))
+            {
+                await motor.ExecuteCommand(command.Command, cancellationTokenSource.Token);
+            }
+        }
+        isCommandProcessing = false;
+    }
+
+    public void CancelOperations()
+    {
+        cancellationTokenSource.Cancel();
+        Console.WriteLine("Cancellation requested.");
+    }
+}
